e9fc4d07562273ee9138714a8acbec2191406268
Arnab Kumar Jha <arnab.jha@enpass.io>
2024-05-07 15:18:34 +0530

    fixed jitters using software rendering

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQuickWindow>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+    QQuickWindow::setGraphicsApi(QSGRendererInterface::Software);
     const QUrl url(QStringLiteral("qrc:/CustomWindowTitleBar/Main.qml"));
     QObject::connect(
         &engine,
